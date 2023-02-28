6b039e79417d2d1d4d6e16f6362948cfe53dbef6
Mega <Mega@TUF15>
2023-02-28 17:57:19 +0200

    Добавьте файлы проекта.

---
+++ Lab1.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using lab;
+
+namespace Lab1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Repline;
+            do
+            {
+                /*Console.WriteLine("input line");
+                string line = Console.ReadLine();
+                SClass test = new SClass(line);
+                Console.WriteLine("String: " + test.ToString());
+                Console.WriteLine("Sorted: " + test.sort());
+                Console.WriteLine("Length: " + test.length());
+                */
+                task();
+                Console.WriteLine("repeat?");
+                Repline = Convert.ToInt32(Console.ReadLine());
+            } while (Repline == 1);
+        }
+        static void task()
+        {
+            Console.WriteLine("input line");
+            string line = Console.ReadLine();
+            SClass test = new SClass(line);
+            Console.WriteLine("String: " + test.ToString());
+            Console.WriteLine("Sorted: " + test.sort());
+            Console.WriteLine("Length: " + test.length());
+        }
+    }
+}
